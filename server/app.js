ff520628e99cfd373090cb607265949fe9790883
vladislavdudal <vladislavdudal@gmail.com>
2020-04-29 04:04:22 +0300

    nuxt-blog-added-routes-jwt-passport

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require ('mongoose')
+const authRoutes = require('./routes/auth.routes')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
+const postRoutes = require('./routes/post.routes')
 const keys = require('./keys')
 const app = express()
 
 
+mongoose.connect(keys.MONGO_URI)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(error => console.error(error))
+
+app.use(passport.initialize())
+passport.use(passportStrategy)
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
 
 module.exports = app
 
